faf37d2f836e9b1971602f10c2ac93ee525ed004
Andrew Sprague <andrewsprague@outlook.com>
2018-07-12 20:59:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
-function destructivelyAppendKitten (arr, element) {
-  arr.push(element)
-  return arr
+function destructivelyAppendKitten (element) {
+  kitten.push(element);
+  return kitten
 }
 
