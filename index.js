35cfebf1d200bf0649188f84d4750dfdc50c0c52
Andrew Sprague <andrewsprague@outlook.com>
2018-07-12 21:05:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten (name) {
-  return [...kittens,name];
+  return [...kittens,name]
+}
+
+function prependKitten (name) {
+  return [name, ...]
 }
